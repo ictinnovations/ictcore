57419edd5ee3ceb94f4fd92d577cd3dc2d850f20
Nasir Iqbal <nasir@ictinnovations.com>
2016-12-12 22:46:26 +0500

    core.php File added again for old compatibility

---
+++ core/core.php
@@ -0,0 +1,10 @@
+<?php
+/* * ***************************************************************
+ * Copyright © 2014 ICT Innovations Pakistan All Rights Reserved   *
+ * Developed By: Nasir Iqbal                                       *
+ * Website : http://www.ictinnovations.com/                        *
+ * Mail : nasir@ictinnovations.com                                 *
+ * *************************************************************** */
+
+// Include the framework
+include_once "Core.php";
