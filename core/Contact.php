18973346429314d4121b83e8e45006b324da9ad7
adeelict <adeel@ictinnovations.com>
2017-10-27 14:09:03 +0500

    delte contact count parts join and leave group

--- core/Contact.php
+++ core/Contact.php
@@ -258,9 +258,9 @@ class Contact
         $result_add = mysql_query("INSERT INTO contact_link(group_id,contact_id) value ($group_id,$this->contact_id)");
         $result = mysql_insert_id();
 
-        $count_contact = mysql_query("SELECT * from contact_link where group_id=".$group_id." GROUP BY contact_id");
+       /* $count_contact = mysql_query("SELECT * from contact_link where group_id=".$group_id." GROUP BY contact_id");
         $cont_result =  mysql_num_rows($count_contact);
-        $udate_group = mysql_query("UPDATE contact_group set contact_count=".$cont_result." where group_id=".$group_id);
+        $udate_group = mysql_query("UPDATE contact_group set contact_count=".$cont_result." where group_id=".$group_id);*/
         Corelog::log("New group contacts created: ", Corelog::CRUD);
       } 
        return $result ;
@@ -269,9 +269,9 @@ class Contact
   {
       $result_add = mysql_query("DELETE from contact_link where contact_id=".$this->contact_id." AND group_id=".$group_id);
       $result =  mysql_affected_rows();
-      $count_contact = mysql_query("SELECT * from contact_link where group_id=".$group_id." GROUP BY contact_id");
-      $cont_result =  mysql_num_rows($count_contact);
-      $udate_group = mysql_query("UPDATE contact_group set contact_count=".$cont_result." where group_id=".$group_id);
+      //$count_contact = mysql_query("SELECT * from contact_link where group_id=".$group_id." GROUP BY contact_id");
+      //$cont_result =  mysql_num_rows($count_contact);
+      //$udate_group = mysql_query("UPDATE contact_group set contact_count=".$cont_result." where group_id=".$group_id);
       Corelog::log("group contacts Deleted: ", Corelog::CRUD);
     return $result ;
   }
