0fbdd5fdbbfe27d65e1e09c6f3ffcdcda7c621f9
adeelict <adeel@ictinnovations.com>
2017-11-02 13:50:44 +0500

    remove extra spces

--- bin/campaign.php
+++ bin/campaign.php
@@ -13,8 +13,6 @@ error_log( "Hello, errors!" );*/
 /* forking */
 (new \Firehed\ProcessControl\Daemon)
  ->setPidFileLocation('/tmp/coreCampaign_td1.pid')
- //->setStdoutFileLocation(sys_get_temp_dir().'/campaign_td.log')
-   // ->setStdErrFileLocation(sys_get_temp_dir().'/campaign_td.log')
  ->setProcessName('coreCampaign')
  ->autoRun();
  //parent close database conection that y i put here
@@ -24,38 +22,37 @@ error_log( "Hello, errors!" );*/
  $oCampaign = new Campaign($campaign_id);
  $get_group_id = $oCampaign->group_id;
  $get_user_id = $oCampaign->created_by;
+ $get_delay = $oCampaign->delay 
  do_login($get_user_id);
-
-
 //transmission create 
-      $query = "SELECT c.first_name,c.last_name,c.phone,c.email,c.contact_id,cl.contact_id ,cl.group_id FROM contact c INNER JOIN contact_link cl ON c.contact_id = cl.contact_id where cl.group_id=".$get_group_id." GROUP BY cl.contact_id";
-      $result_contacts = mysql_query($query);
-      while ($datacontact = mysql_fetch_assoc($result_contacts)) 
+  $query = "SELECT c.first_name,c.last_name,c.phone,c.email,c.contact_id,cl.contact_id ,cl.group_id FROM contact c INNER JOIN contact_link cl ON c.contact_id = cl.contact_id where cl.group_id=".$get_group_id." GROUP BY cl.contact_id";
+  $result_contacts = mysql_query($query);
+  while ($datacontact = mysql_fetch_assoc($result_contacts)) 
+  {
+    //$aGroupcontact[] = $datacontact;
+        $data_transmission = array(
+        'title'            =>'bulk system',
+        'account_id'       =>$get_user_id ,
+        'contact_id'       =>$datacontact['contact_id'],
+        'origin'           =>'',
+        'direction'         =>''
+        );
+        $chk_transmission = mysql_query("select *  from transmission where contact_id=".$datacontact['contact_id']." AND program_id=".$oCampaign->program_id);
+      if(mysql_num_rows($chk_transmission)==0)
       {
-        //$aGroupcontact[] = $datacontact;
-            $data_transmission = array(
-            'title'            =>'bulk system',
-            'account_id'       =>$get_user_id ,
-            'contact_id'       =>$datacontact['contact_id'],
-            'origin'           =>'',
-            'direction'         =>''
-            );
-            $chk_transmission = mysql_query("select *  from transmission where contact_id=".$datacontact['contact_id']." AND program_id=".$oCampaign->program_id);
-          if(mysql_num_rows($chk_transmission)==0)
-          {
-            $oProgram = Program::load($oCampaign->program_id);
-            $direction = empty($data_transmission['direction']) ? Transmission::OUTBOUND : $data_transmission['direction'];
-            $oTransmission = $oProgram->transmission_create($datacontact['contact_id'],$get_user_id , $direction);
-           // $this->set($oTransmission, $data_transmission);
-            if ($oTransmission->save()) 
-            {
-              $trans_id =  $oTransmission->transmission_id;
-            } else 
-            {
-              throw new CoreException(417, 'Transmission creation failed');
-            }
-          }
+        $oProgram = Program::load($oCampaign->program_id);
+        $direction = empty($data_transmission['direction']) ? Transmission::OUTBOUND : $data_transmission['direction'];
+        $oTransmission = $oProgram->transmission_create($datacontact['contact_id'],$get_user_id , $direction);
+       // $this->set($oTransmission, $data_transmission);
+        if ($oTransmission->save()) 
+        {
+          $trans_id =  $oTransmission->transmission_id;
+        } else 
+        {
+          throw new CoreException(417, 'Transmission creation failed');
+        }
       }
+  }
 // Get tranmisions according to group
  $query = "SELECT c.transmission_id,c.program_id,c.contact_id ,c.status,cl.contact_id ,cl.group_id FROM transmission c INNER JOIN contact_link cl ON c.contact_id = cl.contact_id where cl.group_id=".$get_group_id." AND c.program_id=".$oCampaign->program_id." AND c.created_by=".$get_user_id." AND c.status !='processing' GROUP BY cl.contact_id";
  $result = mysql_query($query);
@@ -63,9 +60,10 @@ error_log( "Hello, errors!" );*/
 if(mysql_num_rows($result)>0)
 {
   // Transmissions loop
-   while ($data = mysql_fetch_assoc($result)) 
-    {
-        sleep(2);
+    while ($data = mysql_fetch_assoc($result)){
+       if(is_numeric($get_delay)){
+        usleep($get_delay);
+      }
        try{
              $oTransmission = new Transmission($data['transmission_id']);
              $oTransmission->send() ;
@@ -87,7 +85,6 @@ if(mysql_num_rows($result)>0)
 }
   else
   {
-     // ini_set("log_errors", 1);
       ini_set("error_log", "/tmp/transmission_error.log");
       error_log( "Transmission not found" );
   }
