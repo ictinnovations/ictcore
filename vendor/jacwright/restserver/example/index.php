43dd4827f21971f4bc587aabd4df67cb2b1dc4e8
Nasir Iqbal <nasir@ictinnovations.com>
2017-12-16 12:07:29 +0500

    Authentication improved, JWT support added, CentOs 7 issues fixed in RPM, Update and delete issue fixed for user, security keys added, Vendor folder added to gitignore-2

--- vendor/jacwright/restserver/example/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require __DIR__ . '/../source/Jacwright/RestServer/RestServer.php';
-require 'TestController.php';
-
-$server = new \Jacwright\RestServer\RestServer('debug');
-$server->addClass('TestController');
-$server->handle();
